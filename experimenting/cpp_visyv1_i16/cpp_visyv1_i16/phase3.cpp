0e0fee46d064936a0710b3336390e34e1c902105
Alejandro Castro García <47532786+castro67@users.noreply.github.com>
2022-04-22 17:27:18 +0200

    Parse white space

--- experimenting/cpp_visyv1_i16/cpp_visyv1_i16/phase3.cpp
+++ experimenting/cpp_visyv1_i16/cpp_visyv1_i16/phase3.cpp
@@ -257,9 +257,19 @@ static Token parse_identifier(Source_context& context)
     return token;
 }
 
+static Token parse_white(Source_context& context)
+{
+    auto& src = context.src;
+    Token token{src, Token_type::white};;
+    token.text = plainchar_utf8(u32string() + peek_ch(context));
+    next_ch(context);
+    return token;
+}
+
 unordered_map<char32_t, Parse_token> parse
 {
-    {U'/', parse_div}
+    {U'/', parse_div}, {0x20, parse_white}, {0x09, parse_white},
+    {0x0b, parse_white}, {0xff, parse_white}
 };
 
 static Token parse_token(char32_t start, Source_context& context)
