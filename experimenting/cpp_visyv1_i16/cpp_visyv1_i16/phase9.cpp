b71608a4af3ef4b6ab7d1b3ff3fe390a23fee997
Alejandro Castro García <47532786+castro67@users.noreply.github.com>
2026-01-30 20:00:06 +0100

    Removed copyright dates

--- experimenting/cpp_visyv1_i16/cpp_visyv1_i16/phase9.cpp
+++ experimenting/cpp_visyv1_i16/cpp_visyv1_i16/phase9.cpp
@@ -6,7 +6,7 @@
 /*
 Licensed under the MIT License.
  
-Copyright (c) 2021 Faustic Inferno SL
+Copyright (c) Faustic Inferno SL
  
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
